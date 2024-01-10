e9d8ba9b8bcf454d0bf9af8b52dbca83391b2708
chillymint <yunfu.ye@hand-china.com>
2024-01-11 01:43:18 +0800

    升级swagger011003

--- mallchat-chat-server/src/main/java/com/nb/mallchat/common/user/controller/UserController.java
+++ mallchat-chat-server/src/main/java/com/nb/mallchat/common/user/controller/UserController.java
@@ -1,9 +1,18 @@
 package com.nb.mallchat.common.user.controller;
 
 
+import com.nb.mallchat.common.common.domain.dto.RequestInfo;
+import com.nb.mallchat.common.common.domain.vo.resp.ApiResult;
+import com.nb.mallchat.common.common.intercepter.TokenInterceptor;
+import com.nb.mallchat.common.common.utils.RequestHolder;
+import com.nb.mallchat.common.user.domain.vo.resp.UserInfoResp;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.stereotype.Controller;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * <p>
@@ -11,9 +20,19 @@ import org.springframework.stereotype.Controller;
  * </p>
  *
  */
-@Controller
-@RequestMapping("/user")
+@RestController
+@RequestMapping("/capi/user")
+@Api(value = "用户相关接口")
 public class UserController {
+
+	@GetMapping("/public/userInfo")
+	@ApiOperation("获取用户个人信息")
+	public ApiResult<UserInfoResp> getUserInfo(HttpServletRequest request){
+		// System.out.println(request.getAttribute(TokenInterceptor.UID));
+		RequestInfo requestInfo = RequestHolder.get();
+
+		return null;
+	}
 
 }
 
