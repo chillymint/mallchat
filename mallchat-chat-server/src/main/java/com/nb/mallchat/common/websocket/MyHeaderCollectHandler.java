aea548f96c0c10839c735392098065965aea82ec
yeyunfu <yunfu.ye@hand-china.com>
2024-02-27 23:30:39 +0800

    ip归属地方案上

--- mallchat-chat-server/src/main/java/com/nb/mallchat/common/websocket/MyHeaderCollectHandler.java
+++ mallchat-chat-server/src/main/java/com/nb/mallchat/common/websocket/MyHeaderCollectHandler.java
@@ -4,7 +4,8 @@ import cn.hutool.core.net.url.UrlBuilder;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.codec.http.HttpRequest;
-
+import org.apache.commons.lang3.StringUtils;
+import java.net.InetSocketAddress;
 import java.util.Optional;
 
 /**
@@ -23,6 +24,17 @@ public class MyHeaderCollectHandler  extends ChannelInboundHandlerAdapter {
 			token.ifPresent(s -> NettyUtil.setAttr(ctx.channel(), NettyUtil.TOKEN, s));
 			request.setUri(urlBuilder.getPath().toString());
 
+			//取用户ip
+			String ip = request.headers().get("X-Real-IP");
+			if(StringUtils.isBlank(ip)) {
+				InetSocketAddress address = (InetSocketAddress) ctx.channel().remoteAddress();
+				ip = address.getAddress().getHostAddress();
+			}
+
+			//保存到channel
+			NettyUtil.setAttr(ctx.channel(), NettyUtil.IP, ip);
+			//处理器只需使用一次
+			ctx.pipeline().remove(this);
 		}
 		ctx.fireChannelRead(msg);
 	}
